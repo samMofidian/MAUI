3c367d171632679bb5f1806489490ca6861ac2a8
samMofidian <s.m.a.mofid@gmail.com>
2023-11-18 01:05:59 +0330

    version 1.0

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPageOneClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StackLayout());
+        }      
+        private void OnPageTwoClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FlexLayout());
+        }
+    }
+}
---
+++ StackLayout.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp2
+{
+    public partial class StackLayout : ContentPage
+    {
+
+        public StackLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
